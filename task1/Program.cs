171ee17b86b49bc9afd5eed778952f3eb2839d26
unknown <sayzzer>
2024-03-31 22:17:41 +0300

    Решили первую задачу

---
+++ task1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+class Program
+{
+    static void PrintNumbersRecursively(int M, int N)
+    {
+        if (M > N) return;
+        Console.WriteLine(M);
+        PrintNumbersRecursively(M + 1, N);
+    }
+
+
+    static void Main()
+    {
+        PrintNumbersRecursively(2, 5);
+    }
+}
