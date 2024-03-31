5e07b9bf7e900f289097719de0335d434939a4c7
unknown <sayzzer>
2024-03-31 22:24:35 +0300

    Решили третью задачу

---
+++ task3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+class Program
+{
+    static void PrintArrayReverse(int[] arr, int index)
+    {
+        if (index < 0) return;
+        Console.WriteLine(arr[index]);
+        PrintArrayReverse(arr, index - 1);
+    }
+
+
+    static void Main()
+    {
+        int[] array = { 1, 2, 3, 4, 5 };
+        PrintArrayReverse(array, array.Length - 1);
+    }
+}
